9d5483b9dc3251fb5a1943caabde9cf4b845bcb7
arsAravind <saravindan7871@gmail.com>
2023-12-21 14:41:45 +0530

    Made some changes

--- src/main/java/com/qsp/SpringDemo/EmployeeDao.java
+++ src/main/java/com/qsp/SpringDemo/EmployeeDao.java
@@ -4,5 +4,9 @@ public class EmployeeDao {
 	
 	private int employeeId;
 	private String employeeName;
+	private String employeePhNo;
+	private int employeeId1;
+	private String employeeName1;
+	private String employeePhNo1;
 
 }
